49b5a30a104d3b4ae70ca4595aa51c3c01224bb7
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2021-05-17 18:10:37 -0300

    somete deletando coisas desnecessarias

--- inserts trabalho.sql
+++
@@ -1,9 +0,0 @@
-insert into Usuario (email,nomeUsuario,senha) values ("emailDoLogin@correto.com","User","1234");
-
-insert into Quiz (respostas,idUsuario) values ("A B C D E", 1);
-
-insert into Jogabilidade (idJog,rota,funcao) values (1, "atirador","suporte");
-
-insert into Campeoes (id_jog,nomeCampeao,idcampeao) values (1,"Braum",244);
-
-insert into Administrador (emailAdm,nomeAdm,senha) values ("emailDoAdm@adm.com", "Adm","liberadaABaderna");
